d91702e1d66326713b51adeeb40418dd1c65e25a
rockleona <34214497+rockleona@users.noreply.github.com>
2025-06-10 23:38:58 +0800

    style: format with black

--- src/{project_name}/cli.py
+++ src/{project_name}/cli.py
@@ -1,11 +1,13 @@
 import click
 
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
-@click.option('--name', default='World', help='Name to greet')
+@click.option("--name", default="World", help="Name to greet")
 def greet(name):
     """Greet the user."""
-    click.echo(f'Hello, {name}!')
+    click.echo(f"Hello, {name}!")
